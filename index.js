5d06be081d69130860cc201ff90d8b9eaaf91722
maikynata <maikynata25@gmail.com>
2022-02-16 15:55:35 -0300

    method

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
-    saldo;
+    _saldo;
+
+    sacar(valor){
+        if(this._saldo >=valor){
+            this._saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
+
+const contacorrenteRicardo = new ContaCorrente();
+contacorrenteRicardo.saldo = 0;
+contacorrenteRicardo.agencia = 1001;
+console.log(contacorrenteRicardo.saldo);
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 12345678910;
 cliente1.agencia = 1001;
 cliente1.saldo = 0;
 
-cliente2.nome = "Alice";
-cliente2.cpf = 12345678999;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
-
-console.log(cliente1, cliente2);
+console.log(cliente1);
